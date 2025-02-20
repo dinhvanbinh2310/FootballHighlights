f626c1f4a82275e184828f2f1067599585239379
dinh van binh <dinhvanbinh2310@gmail.com>
2025-02-20 14:54:31 +0700

    fix filter bug

--- src/components/DetailMatch.jsx
+++ src/components/DetailMatch.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const DetailMatch = () => {
     const location = useLocation();
     const { match } = location.state || {};
+    const navigate = useNavigate();
+
+    const handleBackClick = () => {
+        navigate('/'); // Đường dẫn về trang Schedule
+    };
     console.log('mang ne Match Data:', match);
 
     // Kiểm tra nếu không có dữ liệu
@@ -26,24 +32,38 @@ const DetailMatch = () => {
 
     // Hiển thị dữ liệu
     return (
-        <div>
-            <h1>Chi tiết trận đấu</h1>
-            <h2>{homeTeam.name} vs {awayTeam.name}</h2>
-            <p>Ngày: {new Date(date).toLocaleDateString()}</p>
-            <p>Giờ: {new Date(date).toLocaleTimeString()}</p>
-            <p>Địa điểm: {venue.name}, {venue.city}, {venue.country}</p>
-            <p>Sức chứa: {venue.capacity}</p>
-            <p>Trọng tài: {referee.name} ({referee.nationality})</p>
-            <p>Thời tiết: {forecast.status}, {forecast.temperature}</p>
-            <p>Quốc gia: {country.name}</p>
-            <p>Trạng thái: {state.description}</p>
-            <p>Thời gian: {state.clock} phút</p>
-            <p>Tỉ số: {state.score.current}</p>
-            <p>Giải đấu: {league.name} (Mùa giải {league.season})</p>
-            <img src={homeTeam.logo} alt={homeTeam.name} style={{ width: '50px', height: '50px' }} />
-            <img src={awayTeam.logo} alt={awayTeam.name} style={{ width: '50px', height: '50px' }} />
-            <img src={league.logo} alt={league.name} style={{ width: '50px', height: '50px' }} />
-        </div>
+        <>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                <h1>Chi tiết trận đấu</h1>
+                <button onClick={handleBackClick} style={{
+                    padding: '10px 20px',
+                    backgroundColor: '#007bff',
+                    color: 'white',
+                    border: 'none',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                    marginTop: '20px'
+                }}>
+                    Trở lại
+                </button>
+            </div>
+            <div className="centered-content">
+                <h2>{homeTeam.name}<img src={homeTeam.logo} alt={homeTeam.name} style={{ width: '50px', height: '50px' }} /> vs {awayTeam.name}<img src={awayTeam.logo} alt={awayTeam.name} style={{ width: '50px', height: '50px' }} /></h2>
+                <p>Ngày: {new Date(date).toLocaleDateString()}</p>
+                <p>Giờ: {new Date(date).toLocaleTimeString()}</p>
+                <p>Địa điểm: {venue.name}, {venue.city}, {venue.country}</p>
+                <p>Sức chứa: {venue.capacity}</p>
+                <p>Trọng tài: {referee.name} ({referee.nationality})</p>
+                <p>Thời tiết: {forecast.status}, {forecast.temperature}</p>
+                <p>Quốc gia: {country.name}</p>
+                <p>Trạng thái: {state.description}</p>
+                <p>Thời gian: {state.clock} phút</p>
+                <p>Tỉ số: {state.score.current}</p>
+                <p>Giải đấu: {league.name} (Mùa giải {league.season})</p>
+                <img src={league.logo} alt={league.name} style={{ width: '50px', height: '50px' }} />
+            </div>
+        </>
+
     );
 };
 
